936422e086fced6054910300fff8edfb1388fe2a
JosuedavidRH <jrh24555@gmail.com>
2024-11-19 15:40:01 -0500

    v_1.0

---
+++ 2_pos_amarok/modelos/conexion.php
@@ -0,0 +1,18 @@
+<?php
+
+class Conexion{
+
+	static public function conectar(){
+
+
+		$link = new PDO("mysql:host=localhost;dbname=pos-copia-77",
+			            "root",
+			            "");
+
+		$link->exec("set names utf8");
+
+		return $link;
+
+	}
+
+}
